b2c2ac28d3742255fe66ee6f276ac8d3b5858ea0
Danielle Barker - Mac <caseybarker86@gmail.com>
2023-10-11 11:01:22 -0600

    added 2nd button

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function greet(greeting) {
   clickMeButton.innerHTML = "You clicked me!";
   console.log(greeting, "C11!!");
 }
+
+function changeColor(color) {
+  let clickMeButton = document.getElementById("b2");
+  clickMeButton.style.background = color;
+}
